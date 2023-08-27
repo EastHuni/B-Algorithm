e7a30ba2d9d39f9b245867d3c7258d87945c5448
EastHuni <143245630+EastHuni@users.noreply.github.com>
2023-08-27 10:30:45 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int A = in.nextInt();
+
+    if (A >= 90) {
+            System.out.println("A");
+        } else if (A >= 80) {
+        System.out.println("B");
+        
+    }
+    else if (A>=70){
+        System.out.println("C");
+    }
+    else if (A>=60){
+        System.out.println("D");
+    }
+    else {
+        System.out.println("F");
+    }
+   
+    }
+}
