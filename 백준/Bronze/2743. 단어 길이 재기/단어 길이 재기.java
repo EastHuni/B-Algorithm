3a2258ebac2b5b6a5bdcc0415e0267cc17ae563f
EastHuni <143245630+EastHuni@users.noreply.github.com>
2023-11-04 19:27:33 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 208 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String str;
+
+        str = sc.nextLine();
+        char[] arr = str.toCharArray();
+
+        System.out.println(arr.length);
+
+
+
+
+    }
+}
