6a10f3b4aa49403785c6ede228b25f1623fd2375
EastHuni <143245630+EastHuni@users.noreply.github.com>
2023-11-04 22:18:47 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 244 ms, Memory: 18328 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[26];
+
+        for (int i = 0 ; i< arr.length; i++){
+            arr[i] = -1;
+        }
+
+        String str = sc.nextLine();
+
+        for( int i = 0; i < str.length(); i++){
+
+            char ch = str.charAt(i);
+
+            if(arr[ch - 'a'] == -1 ){
+                arr[ch - 'a'] = i;
+            }
+        }
+        for( int val : arr){
+            System.out.print(val + " ");
+        }
+
+    }
+}
