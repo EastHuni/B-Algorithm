52457e61536b8d02bca8b2cdb171d18f8c387b72
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-04-03 22:52:24 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 464 ms, Memory: 40648 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,19 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.net.Inet4Address;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         StringBuilder answer = new StringBuilder();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             while (st.hasMoreTokens()){
                 StringBuilder sb = new StringBuilder(st.nextToken());
@@ -22,5 +19,6 @@ public class Main {
             answer.append('\n');
         }
         System.out.println(answer);
+
     }
 }
