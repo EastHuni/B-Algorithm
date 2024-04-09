2af6ef97beb7b0cc1e13328c1a8225636917d5fd
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-04-10 02:46:35 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,30 +1,26 @@
-import java.io.*;
-import java.sql.SQLOutput;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-
-        int arr[] = new int[26];
-
-        for (int i = 0 ; i< arr.length; i++){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int [] arr = new int[26];
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = -1;
         }
-
-        String str = sc.nextLine();
-
-        for( int i = 0; i < str.length(); i++){
-
-            char ch = str.charAt(i);
-
-            if(arr[ch - 'a'] == -1 ){
-                arr[ch - 'a'] = i;
+        String s = br.readLine();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'a' && ch <= 'z'){
+                arr[ch - 'a'] = s.indexOf(ch);
             }
         }
-        for( int val : arr){
-            System.out.print(val + " ");
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append(' ');
         }
-
+        System.out.println(sb);
     }
 }
