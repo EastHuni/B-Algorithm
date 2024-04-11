248dd383f74b87b199d72b07af6f29e2812f8f95
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-04-11 22:46:06 +0900

    [Bronze III] Title: 팩토리얼, Time: 204 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,13 +1,16 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int result = 1;
-        for (int i = 1; i <= N; i++) {
-            result *= i;
-        }
+        int result = factorial(N);
         System.out.println(result);
     }
+    public static int factorial(int N){
+        if(N <= 1) return 1;
+        return N * factorial(N -1);
+    }
+
 }
