a90459470e8785daa0d48c91dc76e04f0b0effc6
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-04-25 11:23:14 +0900

    [D2] Title: 스도쿠 검증, Time: 196 ms, Memory: 26,844 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,81 +1,73 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
 
-        StringTokenizer st;
-
-        for (int testCase = 1; testCase <= T; testCase++) {
-            int[][] sudoku = new int[10][10];
-            boolean check = true;
-
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int [][] arr = new int[10][10];
+            boolean result = true;
+            
             for (int i = 0; i < 9; i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-
                 for (int j = 0; j < 9; j++) {
-                    sudoku[i][j] = Integer.parseInt(st.nextToken());
+                    arr[i][j] = sc.nextInt();
                 }
             }
-
+            //행 검사  + 열 검사
             for (int i = 0; i < 9; i++) {
-                int[] arr = new int[10];
-
-                for (int j = 0; j < 9; j++)
-                    arr[sudoku[i][j]]++;
-
+                int [] row = new int[10];
+                for (int j = 0; j < 9; j++) {
+                    row[arr[i][j]]++;
+                }
                 for (int k = 1; k <= 9; k++) {
-                    if (arr[k] == 0) {
-                        check = false;
+                    if (row[k] == 0){
+                        result = false;
                         break;
                     }
                 }
             }
 
             for (int i = 0; i < 9; i++) {
-                int[] arr = new int[10];
-
-                for (int j = 0; j < 9; j++)
-                    arr[sudoku[j][i]]++;
-
+                int [] row = new int[10];
+                for (int j = 0; j < 9; j++) {
+                    row[arr[j][i]]++;
+                }
                 for (int k = 1; k <= 9; k++) {
-                    if (arr[k] == 0) {
-                        check = false;
+                    if (row[k] == 0){
+                        result = false;
                         break;
                     }
+
                 }
             }
 
-            for (int i = 0; i <= 6; i += 3) {
-                for (int j = 0; j <= 6; j += 3) {
-                    int[] arr = new int[10];
 
-                    int a = i + 3;
-                    int b = j + 3;
 
-                    for (int x = i; x < a; x++) {
-                        for (int y = j; y < b; y++) {
-                            arr[sudoku[x][y]]++;
+            // 수도쿠 3 x 3 칸 검사  0, 3, 6
+            for (int i = 0; i <= 6; i+=3) {
+                for (int j = 0; j <= 6; j+=3) {
+                    int [] row = new int[10];
+                    int a = i+3;
+                    int b = j+3;
+
+                    for (int k = i; k < a; k++) {
+                        for (int l = j; l < b; l++) {
+                            row[arr[k][l]]++;
                         }
                     }
-
-                    for (int z = 1; z <= 9; z++) {
-                        if (arr[z] == 0) {
-                            check = false;
+                    for (int k = 1; k <= 9; k++) {
+                        if (row[k] == 0){
+                            result = false;
                             break;
                         }
                     }
                 }
             }
+            int answer = result ? 1 : 0;
 
-            int result = check ? 1 : 0;
+            System.out.println("#" + test_case + " " + answer);
 
-            System.out.println("#" + testCase + " " + result);
         }
     }
 }
