8ac693c0d0f2e1ac3b06f67b23fe720c2edcb59e
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-06-10 21:22:36 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = {};
+        answer = new int[2];
+        int a = money / 5500;
+        int b = money % 5500;
+        answer[0] = a; 
+        answer[1] = b;
+        
+        return answer;
+    }
+}
