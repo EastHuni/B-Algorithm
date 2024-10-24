555d8d461064483611955c4c4985ea8aefce1e88
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-10-25 01:24:01 +0900

    [Gold IV] Title: 오큰수, Time: 2096 ms, Memory: 321960 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -5,26 +5,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int [] arr = new int[N];
-        Stack<Integer> stack = new Stack<>();
+        int[] arr = new int[N];
+        Stack<Integer> stack = new Stack<Integer>();
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            while (!stack.isEmpty() && arr[i] > arr[stack.peek()]){
+        for (int i = 0; i <N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]){
                 arr[stack.pop()] = arr[i];
             }
             stack.push(i);
         }
-
-        while(!stack.isEmpty()){
+        while (!stack.isEmpty()){
             arr[stack.pop()] = -1;
         }
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < N; i++) {
             sb.append(arr[i]).append(' ');
         }
         System.out.println(sb);
+
     }
 }
