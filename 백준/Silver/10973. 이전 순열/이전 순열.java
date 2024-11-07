900498f9345f8042c44af8a28033e46f30621f06
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-11-08 02:08:16 +0900

    [Silver III] Title: 이전 순열, Time: 300 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int [] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        if (next()){
+            for (int i = 0; i < N; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }else System.out.println("-1");
+    }
+    public static boolean next(){
+        int point = -1;
+        for (int i = N-1; i > 0 ; i--) {
+            if (arr[i-1] > arr[i]){
+                point = i;
+                break;
+            }
+         }
+        if (point == -1){
+            return false;
+        }
+        int change = -1;
+        for (int i = N-1; i >= point; i--) {
+            if (arr[point-1] > arr[i]){
+                change = i;
+                break;
+            }
+        }
+        swap(point-1, change);
+        change = N-1;
+        while (point<change){
+            swap(point,change);
+            point++;
+            change--;
+        }
+        return true;
+    }
+    public static void swap(int x, int y){
+        int temp = arr[x];
+        arr[x] =arr[y];
+        arr[y] = temp;
+    }
+}
