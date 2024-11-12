9da56d6020b476aeb6372b287cc8131132e952d6
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-11-13 01:21:16 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14804 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean [][] visited;
+    static int [][] graph;
+    static int N,M;
+    static int [] dx = {-1, 1, 0, 0};
+    static int [] dy = {0, 0, -1, 1};
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        graph = new int[N+1][M+1];
+        visited = new boolean[N+1][M+1];
+
+        for (int i = 1; i <= N; i++) {
+            String str = br.readLine();
+            for (int j = 1; j <= M; j++) {
+                graph[i][j] = str.charAt(j-1) - '0';
+            }
+        }
+
+        System.out.println(bfs(1, 1));
+    }
+    public static int bfs(int a, int b){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{a, b, 1});
+        visited[a][b] = true;
+
+        while (!q.isEmpty()){
+            int [] current = q.poll();
+            int x = current[0];
+            int y = current[1];
+            int dist = current[2];
+            if (x == N && y == M) {
+                return dist;
+            }
+            for (int i = 0; i < 4; i++) {
+                int nx = x +dx[i];
+                int ny = y +dy[i];
+                if (nx >= 1 && nx <= N && ny >= 1 && ny <= M && !visited[nx][ny] && graph[nx][ny] == 1){
+                    visited[nx][ny] = true;
+                    q.add(new int[]{nx,ny,dist+1});
+                }
+            }
+
+        }
+        return -1;
+
+    }
+}
