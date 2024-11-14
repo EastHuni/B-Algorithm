e7d3751c857930ceb79b045ae28977df45370f06
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-11-14 23:32:09 +0900

    [D1] Title: 중간값 찾기, Time: 141 ms, Memory: 20,576 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -1,16 +1,17 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         int N = sc.nextInt();
-        int middle = (N+1)/2-1;
+        int a = (N+1)/2-1;
         int [] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
         }
         Arrays.sort(arr);
-        System.out.println(arr[middle]);
+        System.out.println(arr[a]);
     }
 }
