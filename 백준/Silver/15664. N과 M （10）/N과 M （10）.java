d21162ab220c1a25ce2b9ed5b86d9c9a5abd315f
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-11-14 19:21:42 +0900

    [Silver II] Title: N과 M (10), Time: 100 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.java
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -5,19 +5,20 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M;
-    static boolean [] visited;
+    static int N, M;
     static int [] arr;
-    static int [] answer;
+    static int [] result;
+    static boolean [] visited;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        result = new int[M];
         visited = new boolean[N];
-        arr = new int [N];
-        answer = new int[M];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
@@ -25,28 +26,27 @@ public class Main {
         Arrays.sort(arr);
         dfs(0,0);
         System.out.println(sb);
-
     }
-    public static void dfs(int idx, int depth){
-        if(depth == M){
+    public static void dfs(int start, int depth){
+        if (depth == M){
             for (int i = 0; i < M; i++) {
-                sb.append(answer[i]).append(' ');
+                sb.append(result[i]).append(' ');
             }
             sb.append('\n');
             return;
         }
-        int before = 0;
-        for (int i = idx; i < N; i++) {
-
+        int begin = 0;
+        for (int i = start; i < N; i++) {
             if (!visited[i]) {
-                if (before != arr[i]) {
+                if (begin != arr[i]) {
                     visited[i] = true;
-                    before = arr[i];
-                    answer[depth] = arr[i];
+                    begin = arr[i];
+                    result[depth] = arr[i];
                     dfs(i,depth + 1);
                     visited[i] = false;
                 }
             }
         }
     }
+
 }
