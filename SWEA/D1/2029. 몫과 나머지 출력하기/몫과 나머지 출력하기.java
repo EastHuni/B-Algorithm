33e88290d2efe0c524634221732d18c56ecb4fd0
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-11-14 23:20:10 +0900

    [D1] Title: 몫과 나머지 출력하기, Time: 137 ms, Memory: 20,140 KB -BaekjoonHub

--- SWEA/D1/2029. 몫과 나머지 출력하기/몫과 나머지 출력하기.java
+++ SWEA/D1/2029. 몫과 나머지 출력하기/몫과 나머지 출력하기.java
@@ -4,15 +4,11 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        for (int i = 1; i <= T; i++) {
-            int average = 0;
-            int result = 0;
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            average = a / b;
-            result = a % b;
-            System.out.println("#" + i + " " + average + " " + result);
+        int a,b;
+        for (int test_case = 1; test_case <= T; test_case++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println("#" + test_case + " " + (a/b) + " " + (a%b));
         }
-
     }
 }
