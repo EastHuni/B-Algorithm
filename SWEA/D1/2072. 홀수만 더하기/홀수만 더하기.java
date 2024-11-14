cf27ce2f5afbf2d92ada3d8c580042816448fcf0
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-11-14 23:23:29 +0900

    [D1] Title: 홀수만 더하기, Time: 129 ms, Memory: 20,104 KB -BaekjoonHub

--- SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
@@ -1,23 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        for (int i = 0; i < N; i++) {
+        int T = sc.nextInt();
+        int a;
+        for (int test_case = 1; test_case <= T; test_case++) {
             int sum = 0;
-            for (int j = 0; j < 10; j++) {
-                int a = sc.nextInt();
-                if (a % 2 == 1){
-                    sum += a;
-                }
+            for (int i = 0; i < 10; i++) {
+              a = sc.nextInt();
+              if (a % 2 == 1){
+                  sum += a;
+              }
             }
-            System.out.println("#" + (i+1) + " " + sum);
+            System.out.println("#" + test_case + " " + sum);
         }
-
     }
 }
