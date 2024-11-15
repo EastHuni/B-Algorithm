d14afa15ae89f93abe49168922939410c908ada6
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-11-15 23:41:30 +0900

    [Silver III] Title: 모든 순열, Time: 284 ms, Memory: 24976 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    static int [] arr;
     static boolean [] visited;
+    static int [] arr;
     static int N;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        arr = new int[N+1];
         visited = new boolean[N+1];
+        arr = new int[N+1];
         dfs(0);
         System.out.println(sb);
     }
     public static void dfs(int depth){
-        if(depth == N){
+        if (depth == N){
             for (int i = 0; i < N; i++) {
                 sb.append(arr[i]).append(' ');
             }
@@ -25,7 +25,7 @@ public class Main {
             if (!visited[i]){
                 visited[i] = true;
                 arr[depth] = i;
-                dfs(depth+1);
+                dfs(depth + 1);
                 visited[i] = false;
             }
         }
