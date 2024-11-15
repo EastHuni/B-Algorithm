002406b2c19951de1ddd634beefcd6bf61bf0209
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-11-16 00:04:51 +0900

    [Silver II] Title: 차이를 최대로, Time: 124 ms, Memory: 15172 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -1,43 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Inet4Address;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean [] visited;
-    static int [] arr;
-    static int [] answer;
     static int N;
-    static int result;
-    public static void dfs( int depth){
-        if(depth == N){
-            int sum = 0;
-            for (int i = 0; i < N-1; i++) {
-                sum += Math.abs(answer[i] - answer[i+1]);
-            }
-            result = Math.max(result, sum);
-        }
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]){
-                visited[i] = true;
-                answer[depth] = arr[i];
-                dfs(depth + 1);
-                visited[i] = false;
-            }
-        }
-    }
+    static int [] arr;
+    static int [] result;
+    static boolean [] visited;
+    static int max;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
-        answer = new int[N];
         visited = new boolean[N];
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] =Integer.parseInt(st.nextToken());
+        result = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i <N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        dfs(0);
-        System.out.println(result);
+        dfs( 0);
+        System.out.println(max);
+    }
+    public static void dfs(int depth){
+        if (depth == N){
+            int sum = 0;
+            for (int i = 0; i < N-1; i++) {
+                sum += Math.abs(result[i] - result[i+1]);
+            }
+            max = Math.max(max, sum);
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[depth] = arr[i];
+                dfs(depth+1);
+                visited[i] = false;
+            }
+        }
     }
 }
