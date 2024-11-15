919c22a183c9905c8f0e0aa5535131fa835b957a
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-11-16 03:11:40 +0900

    [Gold V] Title: 링크와 스타트, Time: 624 ms, Memory: 21140 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -4,44 +4,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static boolean [] visited;
     static int [][] arr;
-    static int answer;
+    static boolean [] visited;
+    static int N;
+    static int min = Integer.MAX_VALUE;
     static int t;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-        visited = new boolean[N];
-        answer = Integer.MAX_VALUE;
+        arr = new int[N+1][N+1];
+        visited = new boolean[N+1];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         for (t = 1; t < N; t++) {
             dfs(0, 0);
         }
-
-        System.out.println(answer);
+        System.out.println(min);
     }
-    public static void dfs(int idx, int depth){
-
+    public static void dfs(int start, int depth){
         if(depth == t){
-            answer = Math.min(answer, diff());
-            if (answer == 0){
-                System.out.println(answer);
+            min = Math.min(min, diff());
+            if (min == 0){
+                System.out.println(min);
                 System.exit(0);
             }
             return;
         }
-
-        for (int i = idx; i < N; i++) {
+        for (int i = start; i < N; i++) {
             if (!visited[i]){
                 visited[i] = true;
                 dfs(i+1, depth+1);
@@ -49,7 +43,6 @@ public class Main {
             }
         }
     }
-
     public static int diff(){
         int team_start =0;
         int team_link = 0;
