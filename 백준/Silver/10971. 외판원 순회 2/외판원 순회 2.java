b9dac18d246e8a628c92c9dab7c00f01ada6cd81
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-11-16 01:21:52 +0900

    [Silver II] Title: 외판원 순회 2, Time: 136 ms, Memory: 14644 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -4,44 +4,43 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int [][] graph;
-    static boolean [] visited;
     static int N;
+    static int [][]graph;
+    static boolean []visited;
     static int min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-        graph = new int [N][N];
+        graph= new int[N][N];
         visited = new boolean[N];
+
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         for (int i = 0; i < N; i++) {
             visited[i] = true;
-            dfs(i,i,0, 0);
+            dfs(i, i, 0, 0);
         }
         System.out.println(min);
+
     }
-    public static void dfs(int start, int now, int result, int depth){
-        if (depth == N -1){
+    public static void dfs(int start, int now, int sum, int depth){
+        if (depth == N-1){
             if (graph[now][start] != 0){
-                result += graph[now][start];
-                min = Math.min(result, min);
+                sum += graph[now][start];
+                min = Math.min(sum, min);
             }
             return;
         }
-
         for (int i = 0; i < N; i++) {
-            if (!visited[i] && graph[now][i] > 0) {
+            if (!visited[i] && graph[now][i] != 0){
                 visited[i] = true;
-                dfs(start,i,result + graph[now][i], depth+1);
+                dfs(start, i,  sum + graph[now][i], depth + 1);
                 visited[i] = false;
             }
         }
     }
-
 }
