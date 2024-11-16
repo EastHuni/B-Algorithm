f7c2ec440b6c9c7de126bab4cf1d2f4a6694fc2a
EastHuni <143245630+EastHuni@users.noreply.github.com>
2024-11-16 21:29:57 +0900

    [Silver II] Title: 섬의 개수, Time: 144 ms, Memory: 16024 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,56 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int w, h, count;
     static boolean [][] visited;
     static int [][] graph;
-    static int w, h;
+    static int [] dx = {-1, -1, 1, 1, -1, 1, 0 ,0};
+    static int [] dy = {-1, 1, -1, 1, 0, 0, -1 ,1};
     static StringBuilder sb = new StringBuilder();
-    static int [] dx = { 0, 0, -1, 1, -1, 1, -1, 1};
-    static int [] dy = {-1, 1, 0, 0, -1, -1, 1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        while (true) {
+        while(true) {
             st = new StringTokenizer(br.readLine());
             w = Integer.parseInt(st.nextToken());
             h = Integer.parseInt(st.nextToken());
             if (w == 0 && h == 0){
                 break;
             }
-            graph = new int[h][w];
             visited = new boolean[h][w];
+            graph = new int[h][w];
             for (int i = 0; i < h; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < w; j++) {
                     graph[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            int cnt = 0;
+            count = 0;
             for (int i = 0; i < h; i++) {
                 for (int j = 0; j < w; j++) {
                     if (!visited[i][j] && graph[i][j] == 1) {
-                        cnt++;
                         dfs(i, j);
+                        count += 1;
                     }
                 }
             }
-            sb.append(cnt).append('\n');
+            sb.append(count).append('\n');
         }
         System.out.println(sb);
-
     }
-    public static void dfs(int a, int b){
-        visited[a][b] = true;
+    public static void dfs(int x, int y){
+        visited[x][y] = true;
         for (int i = 0; i < 8; i++) {
-            int x = a + dx[i];
-            int y = b + dy[i];
-            if (x >= 0 && x<h && y>=0 && y <w && !visited[x][y] && graph[x][y] == 1){
-                dfs(x,y);
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx>= 0 && nx < h && ny>=0 && ny<w){
+                if(!visited[nx][ny] && graph[nx][ny] == 1){
+                    dfs(nx, ny);
+                }
             }
         }
     }
