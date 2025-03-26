ee35f404292cf0b3dfb0c86df2279dad3d5cf8e1
EastHuni <143245630+EastHuni@users.noreply.github.com>
2025-03-26 21:23:17 +0900

    [Gold V] Title: ABCDE, Time: 224 ms, Memory: 23700 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -5,46 +5,47 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N,M;
     static boolean [] visited;
-    static ArrayList<Integer> A [];
+    static ArrayList <Integer> [] arr;
     static int ans = 0;
-    static int m;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        m = M;
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new ArrayList[N];
         visited = new boolean[N];
-        A = new ArrayList[N];
         for (int i = 0; i < N; i++) {
-            A[i] = new ArrayList<Integer>();
+            arr[i] = new ArrayList<Integer>();
         }
+        int a, b;
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int l = Integer.parseInt(st.nextToken());
-            A[s].add(l);
-            A[l].add(s);
+            a =  Integer.parseInt(st.nextToken());
+            b =  Integer.parseInt(st.nextToken());
+            arr[a].add(b);
+            arr[b].add(a);
         }
         for (int i = 0; i < N; i++) {
-            if (ans == 0){
-                DFS(i, 1);
+            if (ans == 0) {
+                dfs(i, 1);
             }
         }
         System.out.println(ans);
     }
-    static void DFS(int v , int depth){
-        if (depth == 5) {
+    public static void dfs(int v, int depth){
+        if(depth == 5){
             ans = 1;
             return;
         }
         visited[v] = true;
-        for(int i : A[v]){
+        for(int i: arr[v]){
             if (!visited[i]){
-                DFS(i, depth+1);
+                dfs(i,depth + 1);
             }
         }
-        visited[v] = false;
+        visited[v] =false;
     }
 }
