26d371c269886fdaa2e7237732396b06f24757fa
EastHuni <143245630+EastHuni@users.noreply.github.com>
2025-05-24 11:02:59 +0900

    [D2] Title: 시각 덧셈, Time: 75 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
@@ -1,22 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int test_case = 1; test_case <= T ; test_case++) {
-            int hour = sc.nextInt();
-            int min = sc.nextInt();
-            hour += sc.nextInt();
-            min += sc.nextInt();
-            if (min >= 60){
-                hour += 1;
-                min -= 60;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int [] arr = new int[4];
+            for (int i = 0; i < 4; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
             }
-            if (hour > 12){
-                hour -= 12;
+            arr[0] = arr[0] + arr[2];
+            arr[1] = arr[1] + arr[3];
+            if(arr[0] > 12){
+                arr[0] =  arr[0] - 12;
             }
-            System.out.println("#" + test_case + " " + hour + " " + min);
+            if (arr[1] > 60) {
+                arr[1] = arr[1] - 60;
+                arr[0] = arr[0] + 1;
+            }
+            System.out.println("#" + test_case + ' ' + arr[0] + ' ' + arr[1]);
         }
     }
 }
